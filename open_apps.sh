e64bf1a393f3a1f2e3bc5aa63c4fff4d26a15468
maniacviper <maniacviper@gmail.com>
2015-07-27 21:31:38 +0100

    changed script for home desktop startup

--- open_apps.sh
+++ open_apps.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
 
+#open terminator
+terminator &!
+
 # open intellij
-cd /home/agile/software/idea-IU-141.713.2/bin
-./idea.sh &!
+cd ~/software/android-studio/bin
+./studio.sh &!
 
-# go to development directory 
-cd /home/agile/dev/services
-
-# open slack
-scudcloud &!
+#go home current project directory
+cd ~
 
 # open chrome
 google-chrome &!
 
 # open spotify
 spotify &!
-
-# start vm and display vm details
-#read -p 'Enter vm name <ml5 ubuntu | ml7 ubuntu>: ' vm_name
-#read -p 'Enter vm status <run | shutdown>: ' vm_control 
-
-vm_name="ml5 ubuntu"
-vm_control="run"
-
-if [ "$vm_control" == "run" ];
-then
- VBoxManage startvm "$vm_name" --type headless
- vm_status="$(VBoxManage showvminfo "$vm_name" | grep State: | grep -oP "\brunning\b")"
-else
- VBoxManage controlvm "$vm_name" poweroff
- vm_status="$(VBoxManage showvminfo "$vm_name" | grep State: | grep -oP "\bpowered off\b")"
-fi
-
-if [ "$vm_status" == "running" ];
-then echo "$vm_name" " is running"
-else echo "$vm_name" " is shutdown" 
-fi
