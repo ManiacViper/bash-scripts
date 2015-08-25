63988f057adb781474a4e6d03db12a516bedd790
Kenneth Monteiro <kmonteiro@wiley.com>
2015-08-25 14:42:16 +0100

    commented out opening vm for work laptop

--- open_apps.sh
+++ open_apps.sh
@@ -26,19 +26,19 @@ spotify &!
 #read -p 'Enter vm name <ml5 ubuntu | ml7 ubuntu>: ' vm_name
 #read -p 'Enter vm status <run | shutdown>: ' vm_control 
 
-vm_name="ml7 ubuntu"
-vm_control="run"
+#vm_name="ml7 ubuntu"
+#vm_control="run"
 
-if [ "$vm_control" == "run" ];
-then
- VBoxManage startvm "$vm_name" --type headless
- vm_status="$(VBoxManage showvminfo "$vm_name" | grep State: | grep -oP "\brunning\b")"
-else
- VBoxManage controlvm "$vm_name" poweroff
- vm_status="$(VBoxManage showvminfo "$vm_name" | grep State: | grep -oP "\bpowered off\b")"
-fi
+#if [ "$vm_control" == "run" ];
+#then
+ #VBoxManage startvm "$vm_name" --type headless
+ #vm_status="$(VBoxManage showvminfo "$vm_name" | grep State: | grep -oP "\brunning\b")"
+#else
+ #VBoxManage controlvm "$vm_name" poweroff
+ #vm_status="$(VBoxManage showvminfo "$vm_name" | grep State: | grep -oP "\bpowered off\b")"
+#fi
 
-if [ "$vm_status" == "running" ];
-then echo "$vm_name" " is running"
-else echo "$vm_name" " is shutdown" 
-fi
+#if [ "$vm_status" == "running" ];
+#then echo "$vm_name" " is running"
+#else echo "$vm_name" " is shutdown" 
+#fi
