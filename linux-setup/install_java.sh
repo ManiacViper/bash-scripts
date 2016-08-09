a318a906deeffa4eb48d4a234f619575a8bbd3b6
maniacviper <maniacviper@gmail.com>
2016-08-09 22:04:05 +0100

    changed to new java version

--- linux-setup/install_java.sh
+++ linux-setup/install_java.sh
@@ -2,18 +2,16 @@
 
 cd ~/Downloads
 
-wget http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+wget http://download.oracle.com/otn-pub/java/jdk/8u101-b13/jdk-8u101-linux-x64.tar.gz
 
-tar -xvf ~/Downloads/jdk-7u79-linux-x64.tar.gz
+tar -xvf ~/Downloads/jdk-8u101-linux-x64.tar.gz
 
-sudo mkdir /usr/lib/jvm
-
-sudo mv ~/Downloads/jdk1.7* /usr/lib/jvm/jdk1.7.0
+sudo mv ~/Downloads/jdk1.8* /usr/lib/jvm/jdk1.8.0
 
 
-sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0/bin/javac" 1
-sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0/bin/javaws" 1
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0/bin/javaws" 1
 
 sudo chmod a+x /usr/bin/java
 sudo chmod a+x /usr/bin/javac
