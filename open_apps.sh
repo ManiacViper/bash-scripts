8c1efea64fcc6309c5569ac007ac8ff3d713d52c
maniacviper <maniacviper@gmail.com>
2016-10-07 21:48:38 +0100

    prevented broken or redundant software to be opened

--- open_apps.sh
+++ open_apps.sh
@@ -4,23 +4,12 @@
 terminator &!
 
 # open intellij
-#cd ~/software/android-studio/bin
-#./studio.sh
-cd ~/software/idea-IU-141.1010.3/bin
+cd ~/software/idea-IC/bin
 ./idea.sh &!
 
 # android studio
-cd ~/software/android-studio/bin
-./studio.sh &!
+#cd ~/software/android-studio/bin
+#./studio.sh &!
 
 #go home current project directory
 cd ~
-
-# open chrome
-google-chrome &!
-
-# open spotify
-spotify &!
-
-# open sublime
-sublime-text
