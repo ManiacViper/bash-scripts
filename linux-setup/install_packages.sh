fa1c809268be6d0c7876beda8b6d45854f775e50
maniacviper <maniacviper@gmail.com>
2016-10-07 21:16:57 +0100

    removed ide setup, link keeps changing

--- linux-setup/install_packages.sh
+++ linux-setup/install_packages.sh
@@ -23,6 +23,9 @@ sudo apt-get install maven
 echo "Installing meld"
 sudo apt-get install meld
 
+echo "Installing terminator"
+sudo apt-get install terminator
+
 echo "Installing log viewer - glogg"
 sudo apt-get install glogg
 
