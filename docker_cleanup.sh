a7c5dbca0aeb17b631d102a0aaa9e4fd201e2942
kenneth.monteiro <kenneth.monteiro@shazam.com>
2017-03-30 09:43:13 +0100

    added display of docker images after cleanup to confirm removal

--- docker_cleanup.sh
+++ docker_cleanup.sh
@@ -3,4 +3,6 @@ docker stop $(docker ps)
 docker rm $(docker ps -a)
 docker rmi $(docker images)
 docker volume rm $(docker volume ls)
+# check if docker images and processes are removed
+docker images
 docker ps -a
